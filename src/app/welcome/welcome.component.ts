744acd996a2101d53ae36a2501aff40104075771
Blake <lakebayomackway@gmail.com>
2017-05-11 10:03:54 -0700

    add thumbnails, css, final push before installing DB.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 // import { Location } from '@angular/common';
 // import { Animal } from '../animal.model';
 // import { AnimalService } from '../animal.service';
@@ -12,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
   // providers: [AnimalService]
 })
 export class WelcomeComponent implements OnInit {
-  // animalId: number = 3;
+  // animalId: number = null;
   // animalToDisplay: Animal;
 
   constructor() { }
