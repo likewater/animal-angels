04391e22376632a3ab035a0e96116e25515aa1a5
Blake <lakebayomackway@gmail.com>
2017-05-10 18:01:37 -0700

    add images

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, Params } from '@angular/router';
+// import { Location } from '@angular/common';
+// import { Animal } from '../animal.model';
+// import { AnimalService } from '../animal.service';
+
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
+  // providers: [AnimalService]
 })
 export class WelcomeComponent implements OnInit {
+  // animalId: number = 3;
+  // animalToDisplay: Animal;
 
   constructor() { }
 
   ngOnInit() {
+    // this.route.params.forEach((urlParameters) => {
+    //   this.animalId = parseInt(urlParameters['id']);
+    // });
+    // this.animalToDisplay = this.animalService.getAnimalById(this.animalId);
   }
 
 }
